f859362c9c5a1393259600c4eb1a31012c4cbe50
Esha1302 <65848409+Esha1302@users.noreply.github.com>
2020-06-10 21:20:59 +0530

    main program

--- WakeInside.py
+++ WakeInside.py
@@ -1,45 +1,45 @@
-import json
-import speech_recognition as sr
-from playsound import playsound
 from word2number import w2n
-import Functions
-
-with open('./jsondatainside.txt') as o1:
-      data = json.loads(o1.read())
-
-number_of_floors = data['wake']['number of floors']
-print(number_of_floors)
-
-while(True):
-    audio1 = Functions.takeinput() #to listen to the wake word
-    text1 = Functions.recognize(audio1)
-    print("you said:" + text1)
-    k = 0
-
-    if(text1 == data['wake']['keyphrase']):
-        playsound(data['mp3 tracks']['make floor choice'])
-        while(k == 0):
-            audio2 = Functions.takeinput()
-            text2 = Functions.recognize(audio2)
-            try:
-                print("Sphinx thinks you said " + text2)
-                try:
-                    n = w2n.word_to_num(text2)
-                    Functions.floorchange(n)
-                    k = 1
-                except ValueError:
-                    playsound(data['mp3 tracks']['choice again'])
-            except sr.UnknownValueError:
-                playsound(data['mp3 tracks']['error message'])
-                playsound(data['mp3 tracks']['choice again'])
-            except sr.RequestError as e:
-                print("Sphinx error; {0}".format(e))
-
-    elif(text1==data['wake']['stopphrase'] or 'stop' in text1):
-        break;
+from Functions import *
+import time
+p = 1
+while(True and p!=0):
+    time.sleep(5)
+    result, datai = get_configInside()
+    if result == 0:
+        print("File not found")
+        continue
     else:
-        playsound(data['mp3 tracks']['error message'])
-        playsound(data['mp3 tracks']['choice again'])
+        number_of_floors = datai['wake']['number of floors']
+        while(True):
+            audio1 = takeinput() #to listen to the wake word
+            text1 = recognize(audio1)
+            print("you said:" + text1)
+            k = 0
+            if(text1 == datai['wake']['keyphrase']):
+                playsound(datai['mp3 tracks']['make floor choice'])
+                while(k == 0):
+                    audio2 = takeinput()
+                    text2 = recognize(audio2)
+                    try:
+                        print("Sphinx thinks you said " + text2)
+                        try:
+                            n = w2n.word_to_num(text2)
+                            floorchange(n)
+                            k = 1
+                        except ValueError:
+                            playsound(datai['mp3 tracks']['choice again'])
+                            continue
+                    except sr.UnknownValueError:
+                        playsound(datai['mp3 tracks']['error message'])
+                        playsound(datai['mp3 tracks']['choice again'])
+                    except sr.RequestError as e:
+                        print("Sphinx error; {0}".format(e))
+            elif(text1==datai['wake']['stopphrase'] or 'stop' in text1):
+                p = 0
+                break;
+            else:
+                playsound(datai['mp3 tracks']['error message'])
+                playsound(datai['mp3 tracks']['choice again'])
 
 
 
