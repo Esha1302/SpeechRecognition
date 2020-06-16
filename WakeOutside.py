3bc261876bb6fe859970daded8508b3c37676113
Esha1302 <65848409+Esha1302@users.noreply.github.com>
2020-06-16 11:06:24 +0530

    final wakeoutside.py

--- WakeOutside.py
+++ WakeOutside.py
@@ -1,5 +1,5 @@
 import time
-from DeepSpeech.Functions import *
+from Functions import *
 
 
 p = 1
@@ -7,18 +7,16 @@ while (True and p != 0):
     time.sleep(5)
     result, datao = get_configOutside()
     if result == 0:
-        print("File not found")
-        str = playsound("file not found message", datao)
+        playsound("file not found message", datao)
         continue
     else:
         while (True):
             audio1 = takeinput(datao)  # to listen to the wake word
             try:
                 text1 = recognizeSphinx(audio1)
-                print(text1)
                 add_prediction(text1)
                 k = 0
-                if (text1 == datao['wake']['keyphrase'] or 'open' in text1):
+                if (datao['wake']['keyphrase'] in text1):
                     playsound("upordown message", datao)
                     while (k == 0):
                         audio2 = takeinput(datao)
@@ -32,15 +30,19 @@ while (True and p != 0):
                                 k = 1
                                 liftgoesdown(datao)  # pseudo function
                             else:
-                                playsound("choice again messgae", datao)
+                                playsound("choice again message", datao)
 
-                        except sr.UnknownValueError:
+                        except sr.UnknownValueError as e:
+                            logger(e)
                             playsound("error message", datao)
 
                 elif (text1 == datao['wake']['stopphrase'] or 'stop' in text1):
                     p = 0
                     break;
+                else:
+                    playsound("choice again messgae", datao)
 
-            except:
+            except sr.RequestError as e:
+                logger(e)
                 playsound("error message", datao)
                 playsound("choice again message", datao)
