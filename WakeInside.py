75eefa525f2999fd516a6e98cbd55fd4d561552c
Esha1302 <65848409+Esha1302@users.noreply.github.com>
2020-06-16 11:05:55 +0530

    final wakeinside.py

--- WakeInside.py
+++ WakeInside.py
@@ -1,47 +1,52 @@
 from word2number import w2n
-from DeepSpeech.Functions import *
+from Functions import *
 import time
-import playsound
 
 p = 1
 while(True and p!=0):
     time.sleep(5)
     result, datai = get_configInside()
     if result == 0:
-        print("File not found")
+        playsound("file not found message", datai)
         continue
     else:
         number_of_floors = datai['wake']['number of floors']
         while(True):
             audio1 = takeinput() #to listen to the wake word
-            text1 = recognize(audio1)
-            print("you said:" + text1)
-            k = 0
-            if(text1 == datai['wake']['keyphrase']):
-                playsound(datai['mp3 tracks']['make floor choice'])
-                while(k == 0):
-                    audio2 = takeinput()
-                    text2 = recognize(audio2)
-                    try:
-                        print("Sphinx thinks you said " + text2)
+            try:
+                text1 = recognizeSphinx(audio1)
+                add_prediction(text1)
+                k = 0
+                if(datai['wake']['keyphrase'] in text1):
+                    playsound("floor choice message",datai)
+                    while(k == 0):
+                        audio2 = takeinput()
                         try:
-                            n = w2n.word_to_num(text2)
-                            floorchange(n)
-                            k = 1
-                        except ValueError:
-                            playsound(datai['mp3 tracks']['choice again'])
-                            continue
-                    except sr.UnknownValueError:
-                        playsound(datai['mp3 tracks']['error message'])
-                        playsound(datai['mp3 tracks']['choice again'])
-                    except sr.RequestError as e:
-                        print("Sphinx error; {0}".format(e))
-            elif(text1==datai['wake']['stopphrase'] or 'stop' in text1):
-                p = 0
-                break;
-            else:
-                playsound(datai['mp3 tracks']['error message'])
-                playsound(datai['mp3 tracks']['choice again'])
+                            text2 = recognizeDS(audio2)
+                            add_prediction(text2)
+                            try:
+                                n = w2n.word_to_num(text2)
+                                if(n == datai['wake']['current floor']):
+                                    playsound("same floor message",datai)
+                                    continue
+                                else:
+                                    floorchange(n)
+                                    k = 1
+                            except ValueError as e:
+                                logger(e)
+                                playsound("choice again messgae", datai)
+                                continue
+                        except sr.UnknownValueError as e:
+                            logger(e)
+                            playsound("error message", datai)
+                            playsound("choice again message", datai)
+                elif(datai['wake']['stopphrase'] in text1):
+                    p = 0
+                    break;
+                else:
+                    playsound("choice again messgae", datai)
 
-
-
+            except sr.RequestError as e:
+                logger(e)
+                playsound("error message", datai)
+                playsound("choice again message", datai)
