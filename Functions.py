e2c69e71623f8eeac14dd1e77c9bcfa9508074f9
Esha1302 <65848409+Esha1302@users.noreply.github.com>
2020-06-18 09:33:12 +0530

    Final with comments

--- Functions.py
+++ Functions.py
@@ -1,61 +1,59 @@
-import json
-from deepspeech import Model
-import numpy as np
-import speech_recognition as sr
+import json #for loading json data files
+from deepspeech import Model #deepspeech api
+import numpy as np #convert audio frame data into numpy data
+import speech_recognition as sr #wrapper library
 import os
 import datetime
 
 r = sr.Recognizer()
 
-def playsound(message,data):
+def playsound(message,data): #to play saved audio tracks
     str = data['mp3 tracks'][message]
     os.system("mpg123 " + str)
 
-def logger(error_string):
+def logger(error_string): #to append date, time and the error that occured
     with open("Log error files/log.txt", "a+") as log_:
-        log_.write(displaytimedate())
+        log_.write(displaytimedate()[0] + " " +displaytimedate()[1])
         log_.write(" Error: " + error_string + '\n')
 
-def add_prediction(predicted_string):
+def add_prediction(predicted_string): #to append date, time and predicted string
     with open("Log error files/Word prediction file.txt","a+") as pred_:
-        pred_.write(displaytimedate())
+        pred_.write(displaytimedate()[0]+" "+displaytimedate()[1])
         pred_.write(" String predicted is : " + predicted_string + '\n')
 
-def displaytimedate():
+def displaytimedate(): #returns date and time to function call
     now = datetime.datetime.now()
     return("Current date and time : ",now.strftime("%Y-%m-%d %H:%M:%S"))
 
 
-def recognizeDS(audio1):
-    beam_width = 500
-    model_name = "Models/deepspeech-0.7.3-models.pbmm"
-    ds = Model(model_name)
-    ds.setBeamWidth(beam_width)
-    audio1 = np.frombuffer(audio1.frame_data, np.int16)
-    return (ds.stt(audio1))
-
-
-def recognizeSphinx(audio1):
-    return r.recognize_sphinx(audio1)
-
-
-def get_configOutside():
+def get_configOutside(): #loads file for WakeOutside.py
     try:
         with open('Config files/jsondataoutside.txt') as o1:
             dataoutside = json.loads(o1.read())
             return 1, dataoutside
     except Exception as e:
-        logger("error - {}".format(e))
+        logger("error - {}".format(e)) #calls logger function and passes error string
         return 0, 0
 
-
-def takeinput(data):
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        playsound("Speak now message", data)
+def takeinput(data): #takes audio input using pyaudio and speech recognition
+    with sr.Microphone(sample_rate=16000) as source: #16khz is the sample rate expected by models
+        r.adjust_for_ambient_noise(source) #adjusting to background noise
+        playsound("Speak now message", data) #tells user to speak now
         audio = r.listen(source)
-    return audio
+    return audio #returns audio input
 
+
+def recognizeDS(audio1):
+    beam_width = 500 #how many different word sequences will the model take into account
+    model_name = "Models/deepspeech-0.7.3-models.pbmm"
+    ds = Model(model_name)
+    ds.setBeamWidth(beam_width)
+    audio1 = np.frombuffer(audio1.frame_data, np.int16) #converts into numpy array
+    return (ds.stt(audio1)) #returning predicted audio
+
+
+def recognizeSphinx(audio1):
+    return r.recognize_sphinx(audio1) #returning audio via pocketsphinx
 
 def liftgoesup(datao):  # pseudo func
     playsound("up message",datao)
@@ -63,15 +61,15 @@ def liftgoesup(datao):  # pseudo func
 def liftgoesdown(datao):  # psuedo func
     playsound("down message",datao)
 
-
-def get_configInside():
+def get_configInside(): #loads file for WakeInside.py
     try:
         with open('Config files/jsondatainside.txt') as o1:
             datainside = json.loads(o1.read())
             return 1, datainside
     except Exception as e:
-        logger("error - {}".format(e))
+        logger("error - {}".format(e)) #calls logger function and passes error string
         return 0, 0
+
 
 def floorchange(n, datai):  # pseudo func
     print('changing floor to: ', n)
