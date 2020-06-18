9b132f5342aff9049e2a5a1051574fae0f7aa398
Esha1302 <65848409+Esha1302@users.noreply.github.com>
2020-06-18 09:32:21 +0530

    Final with comments

--- WakeOutside.py
+++ WakeOutside.py
@@ -2,47 +2,49 @@ import time
 from Functions import *
 
 
-p = 1
-while (True and p != 0):
-    time.sleep(5)
-    result, datao = get_configOutside()
-    if result == 0:
-        playsound("file not found message", datao)
+p = 1 #control variable to break out of main loop
+while (True and p != 0): #as voice assistant keeps looking for the occcurance of the wake word
+    time.sleep(5) #for it to find the file every 5 seconds
+    result, datao = get_configOutside() #loading file
+    if result == 0: #if file isn't found
+        playsound("file not found message", datao) #giving user the desired message
         continue
-    else:
+    else: #if file is found
         while (True):
             audio1 = takeinput(datao)  # to listen to the wake word
-            try:
-                text1 = recognizeSphinx(audio1)
-                add_prediction(text1)
-                k = 0
-                if (datao['wake']['keyphrase'] in text1):
-                    playsound("upordown message", datao)
+            try: #if it's able to convert wake word into string without error
+                text1 = recognizeSphinx(audio1) #stored result in text1
+                add_prediction(text1) #to add prediction in txt file
+                k = 0 #control variable to break out of second while loop
+                if (datao['wake']['keyphrase'] in text1): #if wake word is present in text1
+                    playsound("upordown message", datao) #tells user to choose whether he wants to go up or down
                     while (k == 0):
-                        audio2 = takeinput(datao)
-                        try:
-                            text2 = recognizeSphinx(audio2)
-                            add_prediction(text2)
-                            if 'up' in text2 or text2 in datao['up']:
+                        audio2 = takeinput(datao) #to take input of his choice
+                        try: #if it's able to recognize choice without error
+                            text2 = recognizeSphinx(audio2) #stored result in text2
+                            add_prediction(text2) #to add prediction in txt file
+                            if 'up' in text2 or text2 in datao['up']: #if choice is up
                                 k = 1
                                 liftgoesup(datao)  # pseudo function
-                            elif 'down' in text2 or text2 in datao['down']:
+                            elif 'down' in text2 or text2 in datao['down']:#if choice is down
                                 k = 1
                                 liftgoesdown(datao)  # pseudo function
-                            else:
-                                playsound("choice again message", datao)
+                            else: #if it's neither up nor down
+                                playsound("choice again message", datao) #tells user to make the choice again
 
-                        except sr.UnknownValueError as e:
-                            logger(e)
-                            playsound("error message", datao)
+                        except sr.UnknownValueError as e: #if sphinx is unable to recognize choice
+                            logger(e) #add error in txt file
+                            playsound("error message", datao) #playing error message
+                            playsound("choice again message", datao) #tells user to make the choice again
 
-                elif (text1 == datao['wake']['stopphrase'] or 'stop' in text1):
+
+                elif (text1 == datao['wake']['stopphrase'] or 'stop' in text1): #if user wants to stop execution of program
                     p = 0
-                    break;
-                else:
-                    playsound("choice again messgae", datao)
+                    break; #breaking out of main loop
+                else: #if the user speaks neither wake word nor stop phrase
+                    playsound("choice again message", datao) #tells user to make choice again
 
-            except sr.RequestError as e:
-                logger(e)
-                playsound("error message", datao)
-                playsound("choice again message", datao)
+            except sr.RequestError as e: #if conversion from speech doesn't take place
+                logger(e) #adding error in txt file
+                playsound("error message", datao)  #playing error message
+                playsound("choice again message", datao) #tells user to make the choice again
